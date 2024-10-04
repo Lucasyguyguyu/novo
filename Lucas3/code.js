7850c46707bc2d77086c826b037959fdbdb93a69
School Dev <112954598+Lucasyguyguyu@users.noreply.github.com>
2024-10-04 11:38:30 -0300

    Add files via upload

---
+++ Lucas3/code.js
@@ -0,0 +1,4 @@
+const quadro = document.querySelectorAll (".quadro")
+const titulo = document.querySelectorAll (".titulo")
+const pergunta = document.querySelectorAll (".pergunta")
+const alternativas = document.querySelectorAll (".alternativas")
